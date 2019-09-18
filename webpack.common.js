47e9a2ccb47a585bd8422c2ba5e60c08c151254e
Kevin <me@bradtravery.com>
2019-09-17 22:10:21 -0400

    environment setup

---
+++ webpack.common.js
@@ -0,0 +1,71 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: {
+    main: './src/index.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        loader: "babel-loader",
+        exclude: /(node_modules|bower_components)/,
+        options: { presets: ["@babel/env"] }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              // you can specify a publicPath here
+              // by default it uses publicPath in webpackOptions.output
+              publicPath: '../',
+              hmr: process.env.NODE_ENV === 'development',
+            },
+          },
+          'css-loader',
+        ],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {},
+          },
+        ],
+      },
+    ]
+  },
+  resolve: { modules: ['node_modules', 'src'], extensions: ["*", ".js", ".jsx"] },
+  plugins: [
+    // new CleanWebpackPlugin(['dist/*']) for < v2 versions of CleanWebpackPlugin
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+    new HtmlWebpackPlugin({
+      hash: true,
+      template: './public/index.html',
+      filename: 'index.html',
+    }),
+  ],
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].bundle.js',
+  }
+};
---
+++ webpack.dev.js
@@ -0,0 +1,24 @@
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+const webpack = require("webpack");
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        loader: 'html-loader'
+      },
+    ]
+  },
+  devServer: {
+    contentBase: "./public",
+    publicPath: "/",
+    hot: true
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ], 
+});
