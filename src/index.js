5c3e17a6eeb345554142a7a89c3bda7817a96a82
Kevin <me@bradtravery.com>
2019-09-18 22:24:42 -0400

    add some assets

--- src/index.js
+++ src/index.js
@@ -1,46 +1,10 @@
 import './index.scss';
 import 'bootstrap';
-import {$,jQuery} from 'jquery';
+import $ from "jquery";
 
-window.$ = $;
-window.jQuery = jQuery;
-
-//collapse navbar on scoll
-
-// Initial scroll position
-var scrollState = 0;
-
-// Primary scroll event function
-var scrollDetect = function(home, down, up) {
-  // Current scroll position
-  var currentScroll = scrollTop();
-  if (scrollTop() === 0) {
-    home();
-  } else if (currentScroll > scrollState) {
-    down();
-  } else {
-    up();
-  }
-  // Set previous scroll position
-  scrollState = scrollTop();
-};
-
-var scrollTop = function() {
-	return window.scrollY;
-};
-
-function downAction() {
-  $('navbar-collapse').removeClass('open');
-  $('navbar-collapse').addClass('collapse');
-}
-
-function upAction() {
-  $('navbar-collapse').removeClass('collapse');
-  $('navbar-collapse').addClass('open');
-}
 
 $(document).ready(function() {
-  window.addEventListener("scroll", function() {
-    scrollDetect(homeAction, downAction, upAction);
+  $(window).scroll(function() {
+    
   });
 })
