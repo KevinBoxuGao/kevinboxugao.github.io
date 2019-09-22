82f22c174416ad05ef2be9d64c4935536e2031d6
Kevin <me@bradtravery.com>
2019-09-22 15:09:25 -0400

    transfer 3

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
-import './index.scss';
 import 'bootstrap';
 import $ from "jquery";
 
+import './index.scss';
 
 $(document).ready(function() {
-  $(window).scroll(function() {
-    
+  $(document).scroll(function () {
+    var $nav = $(".navbar");
+    $nav.toggleClass('nav--scroll', $(this).scrollTop() > $nav.height());
   });
+
+  $('.open-overlay').click(function () {
+    $('.nav-overlay').height('100%');
+  });
+
+  $('.close-overlay').click(function () {
+    $('.nav-overlay').height('0');
+  });
+
+  $('.scroll-down-button').click(function() {
+    $('html, body').animate({
+      scrollTop: $("#portfolio").offset().top
+    }, 1000);
+  });
+
 })
+
