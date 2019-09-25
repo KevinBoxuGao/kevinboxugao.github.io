f672d2c2f90ea7a3d5dc6c1bfdd9e8701f814d4b
Kevin <me@bradtravery.com>
2019-09-24 22:09:03 -0400

    basic design mockup complete

--- src/index.js
+++ src/index.js
@@ -4,19 +4,22 @@ import $ from "jquery";
 import './index.scss';
 
 $(document).ready(function() {
+
+  //navigation
   $(document).scroll(function () {
     var $nav = $(".navbar");
     $nav.toggleClass('nav--scroll', $(this).scrollTop() > $nav.height());
   });
 
   $('.open-overlay').click(function () {
-    $('.nav-overlay').height('100%');
+    $('.nav__overlay').height('100%');
   });
 
   $('.close-overlay').click(function () {
-    $('.nav-overlay').height('0');
+    $('.nav__overlay').height('0');
   });
 
+  //scroll link buttons
   $('.scroll-down-button').click(function() {
     $('html, body').animate({
       scrollTop: $("#portfolio").offset().top
